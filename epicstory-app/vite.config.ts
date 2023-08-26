dd2ce6f8a039b13e6f87038b8538a24f8e5f22a5
Mateus Sarmento <omateusmento@gmail.com>
2023-08-25 21:31:27 -0300

    Add port and open settings to vite dev server in epicstory-app

--- epicstory-app/vite.config.ts
+++ epicstory-app/vite.config.ts
@@ -7,6 +7,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  server: {
+    port: 8080,
+    open: true,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
