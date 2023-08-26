9357c64621c2ec30877efb33c20b2d96ab220e82
Mateus Sarmento <omateusmento@gmail.com>
2023-08-25 21:52:58 -0300

    Use global pipe ValidationPipes

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfig } from './app.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(AppConfig);
-  app.enableCors({
-    origin: config.CORS_ORIGINS,
-  });
+  app.enableCors({ origin: config.CORS_ORIGINS });
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   await app.listen(config.APP_PORT);
 }
 
