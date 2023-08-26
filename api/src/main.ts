df50addb28df79b78e1ff6385a3afeae2dd0ba7e
Mateus Sarmento <omateusmento@gmail.com>
2023-08-25 21:51:40 -0300

    Use AppConfig for app port and cors origins

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppConfig } from './app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(AppConfig);
+  app.enableCors({
+    origin: config.CORS_ORIGINS,
+  });
+  await app.listen(config.APP_PORT);
 }
 
 bootstrap();
