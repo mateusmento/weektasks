97fe7cb0aa8b88fc9581a7c084c73f5ffd07cb3e
Mateus Sarmento <omateusmento@gmail.com>
2023-08-25 21:50:01 -0300

    Add AppConfig

---
+++ api/src/app.config.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AppConfig {
+  constructor(private config: ConfigService) {}
+
+  get APP_PORT(): number {
+    return this.config.get('APP_PORT');
+  }
+
+  get CORS_ORIGINS(): string[] {
+    return this.config
+      .get('CORS_ORIGINS')
+      .split(',')
+      .map((url) => url.trim());
+  }
+}
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AppConfig } from './app.config';
 
 @Module({
   imports: [
@@ -7,5 +8,6 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
   ],
+  providers: [AppConfig],
 })
 export class AppModule {}
