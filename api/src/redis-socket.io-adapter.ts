6fb1e29175c7eb72ac2ef5fd5e82a60c81ad779b
Mateus Sarmento <omateusmento@gmail.com>
2023-08-25 23:12:25 -0300

    Add createIOService method

--- api/src/redis-socket.io-adapter.ts
+++ api/src/redis-socket.io-adapter.ts
@@ -1,8 +1,27 @@
 import { INestApplication } from '@nestjs/common';
 import { IoAdapter } from '@nestjs/platform-socket.io';
+import { Server, ServerOptions } from 'socket.io';
+import { AppConfig } from './app.config';
+import { RedisAdapter } from '@socket.io/redis-adapter';
 
 export class RedisSocketIoAdapter extends IoAdapter {
-  constructor(app: INestApplication) {
+  constructor(
+    app: INestApplication,
+    private config: AppConfig,
+    private adapter: (nsp: any) => RedisAdapter
+  ) {
     super(app);
+  }
+
+  createIOServer(port: number, options?: any) {
+    const server: Server = super.createIOServer(port, {
+      ...options,
+      cors: {
+        origin: this.config.CORS_ORIGINS,
+        credentials: true,
+      },
+    } as ServerOptions);
+    server.use(this.adapter);
+    return server;
   }
 }
