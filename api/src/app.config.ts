3787345198b78b5bcef72a0882bae92ec3196fae
Mateus Sarmento <omateusmento@gmail.com>
2023-08-25 22:48:00 -0300

    Assign config accessors to properties

--- api/src/app.config.ts
+++ api/src/app.config.ts
@@ -3,10 +3,25 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppConfig {
-  constructor(private config = new ConfigService()) {}
+  APP_PORT: number = 0;
+  COOKIE_SECRET: string = '';
+  DATABASE_HOST: string = '';
+  DATABASE_PORT: number = 0;
+  POSTGRES_DB: string = '';
+  POSTGRES_USER: string = '';
+  POSTGRES_PASSWORD: string = '';
 
-  get APP_PORT(): number {
-    return this.config.get('APP_PORT');
+  constructor(private config: ConfigService) {
+    function get(key: string) {
+      return config.get(key);
+    }
+
+    const keys = Object.getOwnPropertyNames(this).filter(
+      (k) => !['config'].includes(k)
+    );
+
+    for (const key of keys)
+      Object.defineProperty(this, key, { get: () => get(key) });
   }
 
   get CORS_ORIGINS(): string[] {
@@ -14,9 +29,5 @@ export class AppConfig {
       .get('CORS_ORIGINS')
       .split(',')
       .map((url) => url.trim());
-  }
-
-  get COOKIE_SECRET(): string {
-    return this.config.get('COOKIE_SECRET');
   }
 }
