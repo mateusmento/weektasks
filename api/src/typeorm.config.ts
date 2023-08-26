670bfe77e84c3927fb5310adcd42b3610f74f530
Mateus Sarmento <omateusmento@gmail.com>
2023-08-25 22:54:41 -0300

    Add typeorm config

---
+++ api/src/typeorm.config.ts
@@ -0,0 +1,32 @@
+import { config as dotenv } from 'dotenv';
+import { DataSourceOptions } from 'typeorm';
+import { AppConfig } from './app.config';
+import { ConfigService } from '@nestjs/config';
+import { readdirSync } from 'fs';
+import path from 'path';
+
+dotenv();
+
+const config = new AppConfig(new ConfigService());
+
+export const typeormConfig: DataSourceOptions = {
+  type: 'postgres',
+  host: config.DATABASE_HOST,
+  port: config.DATABASE_PORT,
+  database: config.POSTGRES_DB,
+  username: config.POSTGRES_USER,
+  password: config.POSTGRES_PASSWORD,
+  logging: true,
+  logger: 'advanced-console',
+  entities: [],
+  migrations: migrations(),
+};
+
+function migrations(): any[] {
+  const migrationPath = path.resolve(__dirname, '../migrations');
+  return readdirSync(migrationPath)
+    .filter((filename) => /[0-9]*-Migration.*(?<!\.d)\.(ts|js)/.test(filename))
+    .map((filename) => path.parse(filename).name)
+    .map((name) => require(`${migrationPath}/${name}`))
+    .flatMap((module) => Object.values(module));
+}
