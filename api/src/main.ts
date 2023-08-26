4201a29bfd0032bbbf3f0051b61a8611ad8832dd
Mateus Sarmento <omateusmento@gmail.com>
2023-08-25 21:56:33 -0300

    Use ClassSerializerInterceptor

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,13 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfig } from './app.config';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(AppConfig);
   app.enableCors({ origin: config.CORS_ORIGINS });
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   await app.listen(config.APP_PORT);
 }
 
