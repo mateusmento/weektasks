ceb0937ae90f9229025d6b8fe18c417e99edaff8
Mateus Sarmento <omateusmento@gmail.com>
2023-08-26 09:35:15 -0300

    Setup file based routing with unplugin-vue-router

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,12 +1,18 @@
 import { fileURLToPath, URL } from 'node:url';
-
+import VueRouter from 'unplugin-vue-router/vite';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    VueRouter({
+      dts: './definitions/typed-router.d.ts',
+    }),
+    vue(),
+    vueJsx(),
+  ],
   server: {
     port: 8080,
     open: true,
