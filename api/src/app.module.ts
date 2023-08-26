be4a76e9f334af023d7ad2d005c2419288fe38f5
Mateus Sarmento <omateusmento@gmail.com>
2023-08-25 22:54:53 -0300

    Use TypeOrmModule in AppModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppConfig } from './app.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './typeorm.config';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => typeormConfig,
+      dataSourceFactory: async (config) => new DataSource(config),
     }),
   ],
   providers: [AppConfig],
