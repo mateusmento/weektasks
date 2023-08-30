d7e627b7766b4d43acb9ea77b3dfbb0e08b96a17
Mateus Sarmento <mateus.sarmento@qikserve.com>
2023-08-30 17:21:30 -0300

    Remove setup of unplugin-vue-router

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,18 +1,11 @@
 import { fileURLToPath, URL } from 'node:url';
-import VueRouter from 'unplugin-vue-router/vite';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    VueRouter({
-      dts: './declarations/typed-router.d.ts',
-    }),
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   server: {
     port: 8080,
     open: true,
