dfc842a3f622f11491804b5396346fd083c3cf0c
Mateus Sarmento <mateus.sarmento@qikserve.com>
2023-09-13 09:34:12 -0300

    Decrease padding of discussion in TimelineView

--- app/src/modules/timeline/TimelineView.vue
+++ app/src/modules/timeline/TimelineView.vue
@@ -48,7 +48,7 @@ function addDiscussion($event: any) {
       </ul>
       <ul class="discussions">
         <li v-for="(discussion, i) in discussions" :key="discussion.id">
-          <Discussion v-model:discussion="discussions[i]" class="p-lg" />
+          <Discussion v-model:discussion="discussions[i]" class="p-md" />
         </li>
         <li class="bottom-item"></li>
       </ul>
